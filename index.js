51306ef9ab7cf2511c1d8134efc626c1be9cf1a2
Leah Davis <ileahd96@gmail.com>
2021-10-12 21:34:57 -0400

    Functions in index.js and generateMarkdown successfully build README and save to root file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
@@ -15,20 +14,61 @@ const questions = [
         type: 'input',
         name: 'email',
         message: 'What is your e-mail address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your projects name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?'
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd()))
-}
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log('Success! Your README file has been created.')
+
+)}
+
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+    inquirer   
+        .prompt(questions)
         .then((answers) => {
-            console.log(answers);
-            writeToFile();
+            writeToFile('README.md', answers)
         })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,105 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+    if (data == 'MIT') {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+
+    } else if (data == 'APACHE 2.0') {
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+
+    } else if (data == 'GPL 3.0') {
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]`;
+
+    } else if (data == 'BSD 3') {
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+
+    } else if (data == 'None') {
+      return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  if (data == 'MIT') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/MIT)`;
+
+  } else if (data == 'APACHE 2.0') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/Apache-2.0)`;
+
+  } else if (data == 'GPL 3.0') {
+    return `${renderLicenseBadge}(https://www.gnu.org/licenses/gpl-3.0)`;
+
+  } else if (data == 'BSD 3') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/BSD-3-Clause)`;
+
+  } else if (data == 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+    renderLicenseLink();
+    if (data === '') {
+      return '';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+  let markdownRender = `
+  # ${data.title}
+
+  ![License Badge](https://img.shields.io/static/v1?label=License&message=${data.license.split(' ').join('-')}&color=success)
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+  
+  ## Installation
+  To install the necessary dependencies for this project, runt he following command:
+
+  \`\`\`
+  ${data.install}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This application is licensed under ${data.license}.
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Testing
+
+  ${data.testing}
+
+  ---
+  
+  ## Questions?
+  For any questions, please contact me via e-mail. Thank you!
+  GitHub: ${data.github}
+  E-mail: ${data.email}
+  
 `;
+  return markdownRender;
 }
 
 module.exports = generateMarkdown;
